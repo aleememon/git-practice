7947e53f9a0a9b6e3dc6e1aad8736d545030b0b2
aleememon <am7359681@gmail.com>
2026-01-28 22:21:03 +0500

    squash the feature/a into main

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ function add(x, y) {
   return x + y;
 }
 
+function sub(a, b) {
+  if (!(a > b)) throw new Error("A should be greater than B")
+  return a - b;
+}
+
 add(5, 5);
 add(4, 10);
 add(5,9);
+
+sub(10,5);
